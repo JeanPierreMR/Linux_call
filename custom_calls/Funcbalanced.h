a00c65055c1538ea6f39bb976023a295e25d595d
JP <jpmejicanos@ufm.edu>
2022-03-10 19:02:35 -0600

    adding all files changed

---
+++ custom_calls/Funcbalanced.h
@@ -0,0 +1,56 @@
+#include <linux/slab.h>
+
+struct sNode {
+    char data;
+    struct sNode* next;
+};
+
+long isMatchingPair(char character1, char character2)
+{
+    if (character1 == '(' && character2 == ')')
+        return 1;
+    else if (character1 == '{' && character2 == '}')
+        return 1;
+    else if (character1 == '[' && character2 == ']')
+        return 1;
+    else
+        return 0;
+}
+
+void push(struct sNode** top_ref, int new_data)
+{
+    // allocate node
+    struct sNode* new_node
+        = (struct sNode*)kmalloc(sizeof(struct sNode),GFP_KERNEL);
+ 
+    if (new_node == NULL) {
+	return;
+	}
+
+	// put in the data
+    new_node->data = new_data;
+
+// link the old list off the new node
+    new_node->next = (*top_ref);
+
+// move the head to point to the new node
+    (*top_ref) = new_node;
+}
+	 
+// Function to pop an item from stack
+int pop(struct sNode** top_ref){
+	char res;
+	struct sNode* top;
+
+	// If stack is empty then error
+	if (*top_ref == NULL) {
+		return res;
+	}
+	else {
+		top = *top_ref;
+		res = top->data;
+		*top_ref = top->next;
+		kfree(top);
+		return res;
+	}
+}
---
+++ custom_calls/check_complete.c
@@ -0,0 +1,49 @@
+#include <linux/kernel.h>
+#include "Funcbalanced.h"
+
+asmlinkage long sys_check_complete(char *exp)
+{
+    // Declare an empty character stack
+    struct sNode* stack = NULL;
+
+    // Traverse the given expression to check matching
+    // brackets
+    while (*exp != '\0')
+    {
+        // If the exp[i] is a starting bracket then push
+        // it
+        if (*exp == '{' || *exp == '(' || *exp == '[')
+            push(&stack, *exp);
+
+        // If exp[i] is an ending bracket then pop from
+        // stack and check if the popped bracket is a
+        // matching pair*/
+        if (*exp == '}' || *exp == ')'
+            || *exp == ']') {
+
+            // If we see an ending bracket without a pair
+            // then return false
+            if (stack == NULL)
+                return 0;
+
+            // Pop the top element from stack, if it is not
+            // a pair bracket of character then there is a
+            // mismatch.
+            // his happens for expressions like {(})
+            else if (!isMatchingPair(pop(&stack), *exp))
+                return 0;
+        }
+        exp++;
+    }
+
+    // If there is something left in expression then there
+    // is a starting bracket without a closing
+    // bracket
+    if (stack == NULL)
+    {
+        return 1; // balanced
+    }
+    else
+        return 0; // not balanced
+}
+
